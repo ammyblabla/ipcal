4e0de9f3c880821864072b110e08cb8503efbd51
ammyblabla <you@example.com>
2017-11-13 00:05:20 +0700

    connect css with html

--- main/views.py
+++ main/views.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
+def index(request):
+    context = {}
+    return render(request, 'index.html', context)
+
+def result(request):
+    context = {}
+    return render(request, 'result.html', context)
